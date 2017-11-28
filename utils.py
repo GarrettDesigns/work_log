ebd0a2bce3be0c1db8fcf4b83f8cb6fe068359d5
Garrett Kucinski <garrettryan@doejo.com>
2017-11-28 12:21:28 -0600

    added task creation prompts

--- utils.py
+++ utils.py
@@ -1,35 +1,7 @@
 """The utility file for storage of common utility methods and variables"""
 
 
-def display_main_menu():
-    """Display the menu for the worklog.
-
-    User can select between options to search for records or create a new one.
-    """
-    while True:
-        choice = input(
-            "\n[C]reate a new entry\n[S]earch existing entries\n[Q]uit\n------"
-            "-------------------------\nPlease make your selection: ").lower()
-
-        if choice == 's':
-            display_search_menu()
-            break
-        elif choice == 'q':
-            break
-
-
-def display_search_menu():
-    """Display the search submenu for the worklog.
-
-    User can select between options to search for records by date,
-    time spent, and exact keyword match, regular expression or return
-    to the main menu.
-    """
-    while True:
-        search = input(
-            "Would you like to search by\n[D]ate\n[T]ime Spent\n[E]xact Match"
-            "\n[R]egEx\nor select [M]ain Menu? ").lower()
-
-        if search == 'm':
-            display_main_menu()
-            break
+def clear_screen():
+    '''Method providing a way to clear the screen after
+       interface selections'''
+    print("\033c", end="")
--- worklog.py
+++ worklog.py
@@ -1,13 +1,13 @@
 """Module contains the worklog object."""
 
-import utils
+import menus
 
 
 class WorkLog:
     """The worklog class representing the work log and all of its data."""
 
     def __init__(self):
-        utils.display_main_menu()
+        menus.display_main_menu()
 
 
 if __name__ == '__main__':
